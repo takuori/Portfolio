72f3a85fe478104318c215e1eeac357ed9161408
takuori <mcz.siorinkanako0604@gmail.com>
2022-07-06 07:28:01 +0000

    投稿(タグ付き)機能(仮)追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,22 +1,53 @@
 class Public::PostsController < ApplicationController
+  def new
+    @post = Post.new
+    @post.tag_posts.build
+  end
+
   def index
+    @tag_list = Tag.all
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @post_tags - @post.tags
   end
 
   def edit
-  end
-
-  def new
+    @post = Post.find(params[:id])
+    @tag_list = @post.tags.pluck(:name).join(',')
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.member_id = current_member.id
+    tag_list = params[:post][:name].split(",")
+    if @post.save
+      @post.tags_save(tag_list)
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    tag_list = params[:post][:name].split(',')
+    if @post.update(post_params)
+       @post.save_tag(tag_list)
+       redirect_to post_path(@post.id),notice: '投稿完了いたしました'
+    else
+       render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:location, :detail, :post_image, {tag_ids: []}).merge(member_id: current_member.id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,40 @@ class Post < ApplicationRecord
 
   has_one_attached :post_image
 
+  def get_post_image
+    if post_image.attached?
+      #file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      #image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      post_image
+    else
+      'no_image.jpg'
+    end
+  end
+
+  def tags_save(tag_list)
+    #current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    if self.tags != nil
+      post_tags_records = TagPost.where(post_id: self.id)
+      post_tags_records.destroy_all
+    end
+
+    tag_list.each do |tag|
+      inspected_tags = Tag.where(name: tag).first_or_create
+      self.tags << inspected_tags
+    end
+    #old_tags = current_tags - sent_tags
+    #new_tags = sent_tags - current_tags
+
+    #old_tags.each do |old|
+      #self.tag_posts.delete Tag.find_by(name: old)
+    #end
+
+    #new_tags.each do |new|
+      #new_tag_posts = Tag.find_or_create_by(name: new)
+      #self.tag_posts << new_tag_posts
+    #end
+  end
+
   def liked_by?(member)
     likes.where(member_id: member.id).exists?
   end
