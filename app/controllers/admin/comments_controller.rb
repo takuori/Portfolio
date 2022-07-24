7b0a4e6321f9a1f1576b0f7a62283c21d9dc0bb5
takuori <mcz.siorinkanako0604@gmail.com>
2022-07-24 04:22:45 +0000

    管理者コメント一覧表示・検索・削除機能追加

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,4 +1,15 @@
 class Admin::CommentsController < ApplicationController
+
+  def comments
+    @comments = Comment.all
+  end
+
   def destroy
+    @comments = Comment.all
+    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    if comment.destroy
+     flash.now[:danger] = "コメントを削除しました"
+     render :destroy
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,18 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :comment, presence: true
+
+  def self.looks(search, word)
+    if search == "perfect_matuch"
+      @comment = Commnet.where("comment LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @comment = Comment.where("comment LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @comment = Comment.where("comment LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @comment = Comment.where("comment LIKE?", "%#{word}%")
+    else
+      @comment = Comment.all
+    end
+  end
 end
